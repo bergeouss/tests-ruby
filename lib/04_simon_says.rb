1faa800871b568b2afe3221efd3a4f52219c2b3d
bergeouss <bergeouss@gmail.com>
2020-04-13 23:45:23 +0200

    Ended with 04_simon_says

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -7,3 +7,22 @@ end
 def repeat (c)
   return "c"+" "+"c"
 end
+def repeat(a, n=2)
+  r = a*n
+  if n > 2
+    r.insert 10," "
+  end
+  return r.insert 5," "
+end
+def start_of_word (d, e)
+    return d[0,e]
+end
+def first_word (s)
+    return s.partition(" ").first
+end
+def titleize (t)
+    l = %w(end the and a or to)
+    t.capitalize.gsub(/(\w+)/) do |z|
+      l.include?(z) ? z : z.capitalize
+    end
+end
