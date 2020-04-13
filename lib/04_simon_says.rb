bfa48a7d6218c9f57973c67129cd101a9fd0f515
bergeouss <bergeouss@gmail.com>
2020-04-13 23:38:58 +0200

    Modified simon_says

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -1,11 +1,9 @@
-def Simon_says
-  def echo (a)
-     return a
-  end
-  def shout (b)
-     return b.upcase  
-  end
-  def repeat (c)
-    return "c"+" "+"c"
-  end
+def echo (a)
+  return a
+end
+def shout (b)
+  return b.upcase  
+end
+def repeat (c)
+  return "c"+" "+"c"
 end
--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -1,3 +1,3 @@
 def time_string(t)
-  Time.at(t"%H%M%S")
+  return Time.at(t-3600).strftime "%H:%M:%S"
 end
